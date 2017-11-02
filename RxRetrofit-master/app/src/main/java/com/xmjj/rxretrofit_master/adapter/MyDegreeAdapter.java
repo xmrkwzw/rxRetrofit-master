0a4791b5b7eb94eff69bc40fd38b506bbe1ea2be
xmwzw <542791316@qq.com>
2017-11-02 18:21:12 +0800

    自定义view更新

--- RxRetrofit-master/app/src/main/java/com/xmjj/rxretrofit_master/adapter/MyDegreeAdapter.java
+++ RxRetrofit-master/app/src/main/java/com/xmjj/rxretrofit_master/adapter/MyDegreeAdapter.java
@@ -1,7 +1,8 @@
 package com.xmjj.rxretrofit_master.adapter;
 
 import android.graphics.Point;
-import android.util.Log;
+
+import com.xmjj.jujianglibrary.util.logger.Logger;
 
 import static java.lang.Math.PI;
 
@@ -12,6 +13,12 @@ import static java.lang.Math.PI;
  */
 public class MyDegreeAdapter {
 
+	private static final int ANGLE_0 = 0;
+	private static final int ANGLE_90 = 90;
+	private static final int ANGLE_180 = 180;
+	private static final int ANGLE_360 = 360;
+
+
 
 	enum _Quadrant {
 		eQ_NONE,                                    //  在坐标轴上
@@ -21,6 +28,17 @@ public class MyDegreeAdapter {
 		eQ_FOUR                                        //	第四象限
 	}
 
+
+	/**
+	 * @param point
+	 * @return 获得点所在角度（点与坐标轴原点连线与Y正半轴的顺时针夹角）单位为度数
+	 */
+	public static int GetRadianByPos(Point point) {
+		double dAngle = GetRadianByPosEx(point);
+		int result = (int) (dAngle * (360 / (2 * PI)));
+		Logger.d("result = " + result);
+		return result;
+	}
 
 	/**
 	 * @param point
@@ -35,26 +53,16 @@ public class MyDegreeAdapter {
 			if (point.y > 0) {
 				return _Quadrant.eQ_ONE;
 			} else {
-				return _Quadrant.eQ_TWO;
+				return _Quadrant.eQ_FOUR;
 			}
 
 		} else {
 			if (point.y < 0) {
 				return _Quadrant.eQ_THREE;
 			} else {
-				return _Quadrant.eQ_FOUR;
+				return _Quadrant.eQ_TWO;
 			}
 		}
-	}
-
-	/**
-	 * @param point
-	 * @return 获得点所在角度（点与坐标轴原点连线与Y正半轴的顺时针夹角）单位为度数
-	 */
-	public static int GetRadianByPos(Point point) {
-		double dAngle = GetRadianByPosEx(point);
-
-		return (int) (dAngle * (360 / (2 * PI)));
 	}
 
 	/**
@@ -68,9 +76,10 @@ public class MyDegreeAdapter {
 		}
 
 
-		double Sin = point.x / Math.sqrt(point.x * point.x + point.y * point.y);
+		double Sin = point.x  / Math.sqrt(point.x * point.x + point.y * point.y);
 		double dAngle = Math.asin(Sin);
-		Log.d("angle", "angle1 = " + dAngle);
+
+		Logger.d("dAngle = " +Sin+"---"+ dAngle);
 		switch (GetQuadrant(point)) {
 			case eQ_NONE:
 				if (point.x == 0 && point.y == 0) {
@@ -79,9 +88,9 @@ public class MyDegreeAdapter {
 
 				if (point.x == 0) {
 					if (point.y > 0) {
-						return -PI/2;
+						return -PI / 2;
 					} else {
-						return PI/2;
+						return PI / 2;
 					}
 				}
 
@@ -89,31 +98,86 @@ public class MyDegreeAdapter {
 					if (point.x > 0) {
 						return 0;
 					} else {
-						return PI/2;
+						return PI / 2;
 					}
 				}
 
-			break;
+				break;
 			case eQ_ONE:
-				dAngle =  dAngle-PI/2;
+				dAngle = dAngle - PI / 2;
 				break;
 
 			case eQ_TWO:
-				dAngle = PI/2 - dAngle;
+				dAngle = PI / 2 - dAngle;
 
-			break;
+				break;
 			case eQ_THREE:
-				dAngle = PI/2 - dAngle;
+				dAngle = PI / 2 - dAngle;
 
-			break;
+				break;
 			case eQ_FOUR:
 				dAngle += 2 * PI;
-				dAngle = dAngle - PI/2;
+				dAngle = dAngle - PI / 2;
 
-			break;
+				break;
 		}
 
 		return dAngle;
 
+	}
+
+
+	/*以逆时针转为例子 x正半轴对应的是0度角 逆时针旋转为负角度*/
+	public static int getAngle(Point point) {
+
+		double Sin = Math.abs(point.y / Math.sqrt(point.x * point.x + point.y * point.y));
+
+		double dAngle = Math.asin(Sin);
+
+		int degree = (int) Math.toDegrees(dAngle);
+
+		Logger.d("degree = before" + degree);
+		switch (GetQuadrant(point)) {
+			case eQ_NONE:
+				if (point.x == 0 && point.y == 0) {
+					degree = 0 ;
+				}
+
+				if (point.x == 0) {
+					if (point.y > 0) {
+						degree = -ANGLE_90;
+					} else {
+						degree =  ANGLE_90;
+					}
+				}
+
+				if (point.y == 0) {
+					if (point.x > 0) {
+						degree =  ANGLE_0;
+					} else {
+						degree = ANGLE_180;
+					}
+				}
+
+				break;
+			case eQ_ONE:
+				degree = -degree;
+				break;
+
+			case eQ_TWO:
+				degree = -(ANGLE_180-degree);
+
+				break;
+			case eQ_THREE:
+				degree = -(degree+ANGLE_180);
+
+				break;
+			case eQ_FOUR:
+				degree = -(ANGLE_360-degree);
+
+				break;
+		}
+
+		return degree;
 	}
 }
--- RxRetrofit-master/app/src/main/java/com/xmjj/rxretrofit_master/widget/MyView.java
+++ RxRetrofit-master/app/src/main/java/com/xmjj/rxretrofit_master/widget/MyView.java
@@ -1,21 +1,17 @@
 package com.xmjj.rxretrofit_master.widget;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import com.xmjj.jujianglibrary.util.DisplayUtils;
-import com.xmjj.rxretrofit_master.R;
+import com.xmjj.jujianglibrary.util.logger.Logger;
 import com.xmjj.rxretrofit_master.adapter.MyDegreeAdapter;
 
 /**
@@ -28,7 +24,8 @@ public class MyView extends View implements View.OnTouchListener {
 	private int padding = 20;
 	private float centerX, centerY;
 	private float radius;
-	private float angel = 0; //旋转的角度
+	private float mAngel = 0; //分针旋转的角度
+	private float hAngel = 0;//时针旋转的角度
 	private Context context;
 
 	public MyView(Context context) {
@@ -64,8 +61,10 @@ public class MyView extends View implements View.OnTouchListener {
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		drawCircle(canvas);
-		drawLine(canvas);
-		drawBitmap(canvas);
+		drawLines(canvas);
+		drawHours(canvas);
+		drawMinute(canvas);
+		drawCenter(canvas);
 	}
 
 
@@ -73,34 +72,90 @@ public class MyView extends View implements View.OnTouchListener {
 
 		centerX = canvas.getWidth() / 2;
 		centerY = canvas.getHeight() / 2;
-		padding = DisplayUtils.dip2px(context,30);
+		padding = DisplayUtils.dip2px(context, 30);
 
 		radius = canvas.getWidth() / 2 - padding;
 
 		Paint paint = new Paint();
-		paint.setColor(Color.RED);
+		paint.setColor(Color.BLACK);
 		paint.setAntiAlias(true);
 		paint.setStrokeWidth(5);
 		paint.setStyle(Paint.Style.STROKE);
 		canvas.drawCircle(centerX, centerY, radius, paint);
 	}
 
+	/*画表盘*/
+	public void drawLines(Canvas canvas) {
+		Paint paint = new Paint();
+		paint.setColor(Color.BLACK);
+		paint.setAntiAlias(true);
+		paint.setStrokeWidth(5);
 
-	public void drawBitmap(Canvas canvas) {
-		canvas.rotate(angel, centerX, centerY);
-		Bitmap hourBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.crow);
-		canvas.drawBitmap(hourBitmap, canvas.getWidth() / 2, canvas.getHeight() / 2 - hourBitmap.getHeight() / 2, null);
+		int count = 12;
+		int angle = 360 / 12;
+		float startX = canvas.getWidth() - padding;
+		float startY = centerY;
+		float stopX = startX - DisplayUtils.dip2px(context, 30);
+		float stopY = startY;
+
+		/*画时钟刻度*/
+		for (int i = 0; i < count; i++) {
+			canvas.drawLine(startX, startY, stopX, stopY, paint);
+			canvas.rotate(angle, centerX, centerY);
+		}
+		/*将角度调整*/
+		for (int i = 0; i < count; i++) {
+			canvas.rotate(-angle, centerX, centerY);
+		}
+
+		float startX2 = canvas.getWidth() - padding;
+		float startY2 = centerY;
+		float stopX2 = startX - DisplayUtils.dip2px(context, 15);
+		float stopY2 = startY;
+		paint.setStrokeWidth(3);
+		/*画分钟刻度*/
+		int angle2 = 360 / 60;
+		int count2 = 60;
+		for (int i = 0; i < count2; i++) {
+			canvas.drawLine(startX2, startY2, stopX2, stopY2, paint);
+			canvas.rotate(angle2, centerX, centerY);
+		}
+		for (int i = 0; i < count2; i++) {
+			canvas.rotate(-angle2, centerX, centerY);
+		}
+
+
 	}
 
-	private void drawLine(Canvas canvas) {
+	/*画分针*/
+	public void drawMinute(Canvas canvas) {
+		canvas.rotate(mAngel, centerX, centerY);
 		Paint paint = new Paint();
-		paint.setStyle(Paint.Style.STROKE);
 		paint.setColor(Color.RED);
-		paint.setStrokeWidth(3);
-		paint.setPathEffect(new DashPathEffect(new float[]{4, 4}, 0));
-		canvas.drawLine(0, canvas.getHeight() / 2, canvas.getWidth(), canvas.getHeight() / 2, paint);
+		paint.setStrokeWidth(10);
+		float length = canvas.getWidth() - DisplayUtils.dip2px(context, 70);
+		canvas.drawLine(centerX, centerY, length, centerY, paint);
+	}
 
-		canvas.drawLine(canvas.getWidth() / 2, 0, canvas.getWidth() / 2, canvas.getHeight(), paint);
+	/*画时针*/
+	public void drawHours(Canvas canvas) {
+		canvas.rotate(hAngel, centerX, centerY);
+		Paint paint = new Paint();
+		paint.setColor(Color.BLACK);
+		paint.setStrokeWidth(15);
+		float length = canvas.getWidth() - DisplayUtils.dip2px(context, 90);
+		canvas.drawLine(centerX, centerY, length, centerY, paint);
+	}
+
+	/*画中间的圆形*/
+	private void drawCenter(Canvas canvas) {
+		Paint paint = new Paint();
+		paint.setStyle(Paint.Style.FILL);
+		paint.setColor(Color.BLUE);
+		float x = centerX;
+		float y = centerY;
+		float radius = DisplayUtils.dip2px(context, 10);
+		canvas.drawCircle(x, y, radius, paint);
 	}
 
 	/**
@@ -112,9 +167,11 @@ public class MyView extends View implements View.OnTouchListener {
 		int ry = (int) -(y - centerY);
 
 		Point point = new Point(rx, ry);
-		angel = MyDegreeAdapter.GetRadianByPos(point);
-		Log.d("angle","angle2 = "+angel);
+		mAngel = MyDegreeAdapter.getAngle(point);
+		hAngel = mAngel/60;
+		Logger.d("degree = after" + mAngel);
 	}
+
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
